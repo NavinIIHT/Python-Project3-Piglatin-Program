2a2f40aa5f3e0c52c06a282c7cd0d1a038d09b45
NavinIIHT <70938679+NavinIIHT@users.noreply.github.com>
2021-10-27 10:57:47 +0530

    Add files via upload

---
+++ test/test_exceptional.py
@@ -0,0 +1,38 @@
+
+# import colorama
+# from colorama import Fore
+# colorama.init(autoreset=True)
+import unittest
+import sys
+sys.path.append("..")
+from operational_steps import count_steps
+from piglatin import get_piglatin
+class ExceptionalTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        with open("output_exception_revised.txt","w") as f:
+            pass
+    def test_error_type(self):
+        try:
+            count_steps("45")
+            with open("output_exception_revised.txt","a") as f:
+                f.write("TestErrorType=False\n")
+                # print("{}TestErrorType = {}Failed".format(Fore.YELLOW,Fore.RED))
+                print("TestErrorType = Failed")
+        except ValueError:
+            with open("output_exception_revised.txt","a") as f:
+                f.write("TestErrorType=True\n")
+                # print("{}TestErrorType = {}Passed".format(Fore.YELLOW,Fore.GREEN))
+                print("TestErrorType = Passed")
+    def test_error_type_for_piglatin(self):
+        try:
+            get_piglatin(23)
+            with open("output_exception_revised.txt","a") as f:
+                f.write("TestErrorTypeForPiglatin=False\n")
+                # print("{}TestErrorTypeForPiglatin = {}Failed".format(Fore.YELLOW,Fore.RED))
+                print("TestErrorTypeForPiglatin = Failed")
+        except ValueError:
+            with open("output_exception_revised.txt","a") as f:
+                f.write("TestErrorTypeForPiglatin=True\n")
+                # print("{}TestErrorTypeForPiglatin = {}Passed".format(Fore.YELLOW,Fore.GREEN))
+                print("TestErrorTypeForPiglatin = Passed")
---
+++ test/test_functional.py
@@ -0,0 +1,37 @@
+# import colorama
+# from colorama import Fore
+# colorama.init(autoreset=True)
+import unittest
+import sys
+sys.path.append("..")
+from operational_steps import count_steps
+from piglatin import get_piglatin
+class FuctionalTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        with open("output_revised.txt","w") as f:
+            pass
+    def test_return_type_int(self):
+        n=count_steps(25)
+        if type(n)==type(1):
+            with open("output_revised.txt","a") as f:
+                f.write("TestReturnTypeInt=True\n")
+                # print("{}TestReturnTypeInt = {}Passed".format(Fore.YELLOW,Fore.GREEN))
+                print("TestReturnTypeInt = Passed")
+        else:
+            with open("output_revised.txt","a") as f:
+                f.write("TestReturnTypeInt=False\n")
+                # print("{}TestReturnTypeInt = {}Failed".format(Fore.YELLOW,Fore.RED))
+                print("TestReturnTypeInt = Failed")
+    def test_return_type_for_piglatin(self):
+        s=get_piglatin("This is Venu")
+        if type(s)==type(" "):
+            with open("output_revised.txt","a") as f:
+                f.write("TestReturnTypeForPiglatin=True\n")
+                # print("{}TestReturnTypeForPiglatin = {}Passed".format(Fore.YELLOW,Fore.GREEN))
+                print("TestReturnTypeForPiglatin = Passed")
+        else:
+            with open("output_revised.txt","a") as f:
+                f.write("TestReturnTypeForPiglatin=False\n")
+                # print("{}TestReturnTypeForPiglatin = {}Failed".format(Fore.YELLOW,Fore.RED))
+                print("TestReturnTypeForPiglatin = Failed")
