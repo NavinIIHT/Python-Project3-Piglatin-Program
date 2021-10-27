a98138387b3c94a51dc0d6841690b95ebf669517
NavinIIHT <70938679+NavinIIHT@users.noreply.github.com>
2021-10-27 10:58:05 +0530

    Add files via upload

---
+++ operational_steps.py
@@ -0,0 +1,11 @@
+def count_steps(N):
+    pass
+
+
+
+if __name__=="__main__":
+    pass
+
+
+
+#def count_steps(N):\n\rpass if __name__=="__main__":\n\rpass
---
+++ piglatin.py
@@ -0,0 +1,12 @@
+def get_piglatin(text):
+    pass
+
+
+if __name__=="__main__":
+    pass
+
+   
+
+
+
+#def get_piglatin(text):\n\rpass \n\rif __name__=="__main__":\n\rpass
